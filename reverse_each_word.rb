2b5dc9f6aa46cd4b87bb88de1dff71aa4e7d99c2
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-11-14 22:56:42 -0800

    commit tree c800633cb7f5ba23dd814ca9bf595aca4767e557

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,4 +8,13 @@ end
 new_array.join (" ")
  end
 
+# so you have to make a method with one argument 
+# then you have to turn that argument into a string 
+# then make and empty array because you will eventually have to turn your string back into an array but not yet
+# take your do an iteration on your original_array with a do loop 
+#   make a varible between the bars ||
+#   call on your new_array add it to your varible with the .reverse
+# end
+# then call your new_array and turn it back into a string 
+
 
